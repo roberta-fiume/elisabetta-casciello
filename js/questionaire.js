f0fba529b6ba3e85bfaaf88c34ef67030e40fbe2
roberta-fiume <robertaf898@gmail.com>
2018-07-18 13:17:27 +0100

    parse string answers in numbers and store them in array in js. Tries to sum answers but it needs to be edited, it doesn't work yet.

--- js/questionaire.js
+++ js/questionaire.js
@@ -3,22 +3,39 @@ window.addEventListener("load", function() {
     function addEventOnCheckBoxes() {
         var checkedBoxes = document.getElementsByClassName('answer');
         for (var i=0; i< checkedBoxes.length; i++) {
-            checkedBoxes[i].addEventListener('click', getValueFromCheckBox);
+            checkedBoxes[i].addEventListener('click', pushValuesFromCheckBoxInArray);
         }
     }
     addEventOnCheckBoxes();
 
-    function getValueFromCheckBox() {
+
+    function pushValuesFromCheckBoxInArray() {
         var arrayOfAnswers = [];
         var roundCheckes = document.getElementsByTagName("input");
-        console.log(roundCheckes);
         for (var i = 0; i < roundCheckes.length; i++) {
             if(roundCheckes[i].checked == true) {
-                arrayOfAnswers.push(roundCheckes[i].value);
+                var integerAnswers = parseInt(roundCheckes[i].value, 10);
+                arrayOfAnswers.push(integerAnswers);
             } 
         } 
         console.log(arrayOfAnswers);
         return arrayOfAnswers;
     } 
+
+    function sumAnswersInArray() {
+        var sumAnswers = 0;
+        for (var i = 0; i < arrayOfAnswers.length; i++) {
+            sumAnswers+= arrayOfAnswers[i];
+        }
+        console.log(sumAnswers);
+        return sumAnswers;  
+    }
+
+    function addEventToGetResultButton() {
+        var buttonResult = document.getElementById('div-get-result');
+        buttonResult.addEventListener("click", sumAnswersInArray);
+    }
+  
+
 
 });
