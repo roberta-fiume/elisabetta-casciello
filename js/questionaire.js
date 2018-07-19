3494c975a34ba33d90bc92b93eecfc50c62b4ca6
roberta-fiume <robertaf898@gmail.com>
2018-07-19 13:13:49 +0100

    Finishes questionaire Js.

--- js/questionaire.js
+++ js/questionaire.js
@@ -6,9 +6,9 @@ window.addEventListener("load", function() {
             checkedBoxes[i].addEventListener('click', pushValuesFromCheckBoxInArray);
         }
     }
+
     addEventOnCheckBoxes();
-
-
+  
     function pushValuesFromCheckBoxInArray() {
         var arrayOfAnswers = [];
         var roundCheckes = document.getElementsByTagName("input");
@@ -22,20 +22,53 @@ window.addEventListener("load", function() {
         return arrayOfAnswers;
     } 
 
-    function sumAnswersInArray() {
-        var sumAnswers = 0;
-        for (var i = 0; i < arrayOfAnswers.length; i++) {
-            sumAnswers+= arrayOfAnswers[i];
-        }
-        console.log(sumAnswers);
-        return sumAnswers;  
-    }
-
     function addEventToGetResultButton() {
         var buttonResult = document.getElementById('div-get-result');
-        buttonResult.addEventListener("click", sumAnswersInArray);
+        buttonResult.addEventListener("click", defineProfileAndPrintResultinHtml);
     }
+ 
+    function sumAnswersInArray() {
+        var arrayOfAnswers = pushValuesFromCheckBoxInArray();
+        var summedAnswers = 0;
+        for (var i = 0; i < arrayOfAnswers.length; i++) {
+            summedAnswers+= arrayOfAnswers[i];
+        }
+        console.log(summedAnswers);
+        return summedAnswers;  
+    }
+
+    function defineProfileAndPrintResultinHtml() {
+        hideTest();
+        showTestResult();
+        var testResultProfile = document.getElementById("get-result-profile");
+        var summedAnswers = sumAnswersInArray();
+        if (summedAnswers === 14) {
+          return testResultProfile.innerHTML = "Congratulazioni hai totalizzato il massimo punteggio! Hai ottime abitudini alimentari e ti piace prenderti cura di te attraverso una regolare attività fisica e una corretta alimentazione."+ " "+ "Questo ti aiuta a sentirti sempre al meglio giorno dopo giorno. Continua così!!!";
+        } else if (summedAnswers < 14 && summedAnswers >= 8) {
+          returntestResultProfile.innerHTML =  "Siamo sulla buona strada! Le tue abitudini alimentari sono quasi tutte giuste." + " " +  "Ti consiglio di riprendere in considerazione le risposte alle quali hai dato “SI”. Puoi fare di più per te stesso/a.";
+        } else if (summedAnswers === 7) {
+          return  testResultProfile.innerHTML = "Alcune delle tue abitudini alimentari sono corrette ma c’è ancora tanta strada da fare per “correggere il tiro”!" + " " + "Puoi sicuramente lavorarci su per sentirti al meglio.";
+        } else if (summedAnswers >= 0 && summedAnswers < 6) {
+          return  testResultProfile.innerHTML = "Ahimè bisogna correre ai ripari!" + " " + "Il tuo punteggio evidenzia abitudini alimentari scorrette." + " " + "C’è molto da fare ma non scoraggiarti!" + " " + "Non è mai troppo tardi per iniziare a mangiare in modo sano e consapevole.";
+        }
+    }
+    
+    
+    function hideTest() {
+        var divTest = document.getElementById('big-div-group-statements');
+        divTest.style.display = "none";
+        showTestResult();
+    }
+
+    function showTestResult() {
+        var testResult = document.getElementById('div-get-result-profile');
+        testResult.style.display = "flex";
+    }
+
+
+    addEventToGetResultButton();
   
+
 
 
 });
